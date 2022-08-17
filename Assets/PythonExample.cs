2f22b8381e9fd4c78ce3d696439c9c2155b0336e
Slime <etslime@gmail.com>
2022-08-18 05:46:22 +0800

    final version

---
+++ Assets/PythonExample.cs
@@ -0,0 +1,41 @@
+﻿//#region 
+//// D:\UnityProjects\PrivacyGame\Assets\Plugins\MathNet.Numerics.dll
+//#endregion
+
+//using UnityEngine;
+
+//using MathNet.Numerics.LinearAlgebra;
+//using MathNet.Numerics.LinearAlgebra.Double;
+
+//public class PythonExample : MonoBehaviour {
+
+//    // Use this for initialization
+//    void Start()
+//    {
+//        var engine = Python.CreateEngine();
+
+//        ICollection<string> searchPaths = engine.GetSearchPaths();
+
+//        //Path to the folder of greeter.py
+//        searchPaths.Add(Application.dataPath);
+//        //Path to the Python standard library
+//        //searchPaths.Add(Application.dataPath + @"\Python\Lib\");
+//        searchPaths.Add(@"D:\anaconda3\envs\game\Lib");
+//        engine.SetSearchPaths(searchPaths);
+
+//        dynamic py = engine.ExecuteFile(Application.dataPath + @"\Python\greeter.py");
+//        dynamic greeter = py.Greeter("Mika");
+//        Debug.Log(py.main());
+
+//    //verify: the following should be approximately(0, 0, 0)
+//        Player.results = (A * (2 * nullspace[0] - 3 * nullspace[1])).ToString();
+//        Debug.Log(ScenarioCode.scenarios["a"]);
+//        ScenarioCode.Print(ScenarioCode.scenarios["a"]);
+//    }
+
+//    // Update is called once per frame
+//    void Update () 
+//    {
+		
+//	}
+//}
---
+++ Assets/README.txt
@@ -0,0 +1,18 @@
+Please follow the steps below to complete the user study
+
+1. Open Participant Information Sheet( PIS.PDF）
+    and read it carefully.
+
+2. Open the game
+
+3. Open the browser, use the link: https://forms.office.com/r/3KMPr850UZ
+to open the questionnaire.
+
+4.  (optional) use the link: file.io 
+to upload your saved data file, you can find your saved data file in the following directory:
+
+/PrivacyContextGame/PrivacyGame_Data/Save/playerInfo_<yourname>.json
+
+A link will be generated after you uploading the file, just copy the link to somewhere you won't forget
+
+5. Complete the questionnaire
